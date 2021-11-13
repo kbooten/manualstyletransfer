7d44c1627c0576bfc1bf472a9730444f74fc2740
kbooten <7179456+kbooten@users.noreply.github.com>
2021-11-13 05:41:20 -0500

    x's, add more punctuation to exclude

--- SimTester.py
+++ SimTester.py
@@ -4,14 +4,19 @@ from nltk import tokenize
 
 
 try:
-    model = gensim.models.KeyedVectors.load_word2vec_format('tempvectors.bin', binary=True)  ## you need some vectors for this to work
+    custom_vectors = "/Users/kyle/Desktop/GoogleNews-vectors-negative300.bin" ## include custom vectors (large, not for web)
+    model = gensim.models.KeyedVectors.load_word2vec_format(custom_vectors, binary=True)
+    try:
+        model = gensim.models.KeyedVectors.load_word2vec_format('tempvectors.bin', binary=True)  ## you need some vectors for this to work
+    except:
+        import boto3
+        s3 = boto3.resource('s3')
+        KEY = "shrunkenvectors_200000.bin"  ## too big for github so on s3
+        BUCKET_NAME="lang-models"
+        s3.Bucket(BUCKET_NAME).download_file(KEY, 'tempvectors.bin')
+        model = gensim.models.KeyedVectors.load_word2vec_format('tempvectors.bin', binary=True)
 except:
-    import boto3
-    s3 = boto3.resource('s3')
-    KEY = "shrunkenvectors_200000.bin"  ## too big for github so on s3
-    BUCKET_NAME="lang-models"
-    s3.Bucket(BUCKET_NAME).download_file(KEY, 'tempvectors.bin')
-    model = gensim.models.KeyedVectors.load_word2vec_format('tempvectors.bin', binary=True)
+    pass
 
     
 class SimTester:
@@ -21,7 +26,7 @@ class SimTester:
         self.used_sents = []
         
     def custom_tokenize(self,astring):
-        return [w.lower() for w in tokenize.casual_tokenize(astring) if w not in ",!?."]
+        return [w.lower() for w in tokenize.casual_tokenize(astring) if w not in ",!?.-;:"]
       
     def sim_score(self,s1,s2):
         s1_tok = self.custom_tokenize(s1)
